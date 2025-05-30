926cf3f763dc9cd1b8fac0533dbc9d834b0606bf
Nibirh-47 <fatinsadabnibirh@gmail.com>
2025-05-30 17:32:38 +0600

    Add files via upload

---
+++ task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator.")
+city  = input("What's the name of the city you grew up in?\n")
+pets = input("What's your pet's name?\n")
+print("Your band name could be : " + city +" "+ pets )
